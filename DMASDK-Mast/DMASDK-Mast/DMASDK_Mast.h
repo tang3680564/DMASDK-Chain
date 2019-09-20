de6897db97cc4337489270b6da2130ef884c1746
tangjianqiang <494326253@qq.com>
2019-09-20 16:44:21 +0800

    init

---
+++ DMASDK-Mast/DMASDK-Mast/DMASDK_Mast.h
@@ -0,0 +1,19 @@
+//
+//  DMASDK_Mast.h
+//  DMASDK-Mast
+//
+//  Created by StarryMedia 刘晓祥 on 2019/9/20.
+//  Copyright © 2019 starrymedia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DMASDK_Mast.
+FOUNDATION_EXPORT double DMASDK_MastVersionNumber;
+
+//! Project version string for DMASDK_Mast.
+FOUNDATION_EXPORT const unsigned char DMASDK_MastVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DMASDK_Mast/PublicHeader.h>
+
+
