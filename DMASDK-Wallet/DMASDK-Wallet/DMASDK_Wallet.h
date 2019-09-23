0c50a8e0bee5a285d2366efbfab910a5c74ec381
tangjianqiang <494326253@qq.com>
2019-09-23 09:20:43 +0800

    init

---
+++ DMASDK-Wallet/DMASDK-Wallet/DMASDK_Wallet.h
@@ -0,0 +1,19 @@
+//
+//  DMASDK_Wallet.h
+//  DMASDK-Wallet
+//
+//  Created by StarryMedia 刘晓祥 on 2019/9/20.
+//  Copyright © 2019 starrymedia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DMASDK_Wallet.
+FOUNDATION_EXPORT double DMASDK_WalletVersionNumber;
+
+//! Project version string for DMASDK_Wallet.
+FOUNDATION_EXPORT const unsigned char DMASDK_WalletVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DMASDK_Wallet/PublicHeader.h>
+
+
