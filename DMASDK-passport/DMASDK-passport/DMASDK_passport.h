4a28475fb8069963761cb25b32392e5668e542d0
tangjianqiang <494326253@qq.com>
2019-09-28 13:17:10 +0800

    init

---
+++ DMASDK-passport/DMASDK-passport/DMASDK_passport.h
@@ -0,0 +1,19 @@
+//
+//  DMASDK_passport.h
+//  DMASDK-passport
+//
+//  Created by StarryMedia 刘晓祥 on 2019/9/28.
+//  Copyright © 2019 starrymedia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DMASDK_passport.
+FOUNDATION_EXPORT double DMASDK_passportVersionNumber;
+
+//! Project version string for DMASDK_passport.
+FOUNDATION_EXPORT const unsigned char DMASDK_passportVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DMASDK_passport/PublicHeader.h>
+
+
