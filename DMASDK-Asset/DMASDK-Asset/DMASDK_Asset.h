53f5e7b138e866503e272507dd6d1cb00861e298
tangjianqiang <494326253@qq.com>
2019-09-28 11:39:21 +0800

    fix

---
+++ DMASDK-Asset/DMASDK-Asset/DMASDK_Asset.h
@@ -0,0 +1,19 @@
+//
+//  DMASDK_Asset.h
+//  DMASDK-Asset
+//
+//  Created by StarryMedia 刘晓祥 on 2019/9/28.
+//  Copyright © 2019 starrymedia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DMASDK_Asset.
+FOUNDATION_EXPORT double DMASDK_AssetVersionNumber;
+
+//! Project version string for DMASDK_Asset.
+FOUNDATION_EXPORT const unsigned char DMASDK_AssetVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DMASDK_Asset/PublicHeader.h>
+
+
