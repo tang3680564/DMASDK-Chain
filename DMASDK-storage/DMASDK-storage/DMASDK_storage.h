bc2ee95f323d50c397aa9c9113d6f8a688d1f8d4
tangjianqiang <494326253@qq.com>
2019-09-28 11:57:32 +0800

    init

---
+++ DMASDK-storage/DMASDK-storage/DMASDK_storage.h
@@ -0,0 +1,19 @@
+//
+//  DMASDK_storage.h
+//  DMASDK-storage
+//
+//  Created by StarryMedia 刘晓祥 on 2019/9/28.
+//  Copyright © 2019 starrymedia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DMASDK_storage.
+FOUNDATION_EXPORT double DMASDK_storageVersionNumber;
+
+//! Project version string for DMASDK_storage.
+FOUNDATION_EXPORT const unsigned char DMASDK_storageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DMASDK_storage/PublicHeader.h>
+
+
